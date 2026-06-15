9c754534e6652f4504169f231d6833cc7cc69cbf
Mr-Neutr0n <64578610+Mr-Neutr0n@users.noreply.github.com>
2026-06-15 10:53:46 +0000

    Fix #708: Add vercel analytics to our codegraphcontext.vercel.app webs
    Signed-off-by: Mr-Neutr0n <64578610+Mr-Neutr0n@users.noreply.github.com>

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -22,6 +23,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <QueryClientProvider client={queryClient}>
+        <Analytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
